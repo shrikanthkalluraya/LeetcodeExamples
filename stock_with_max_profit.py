fc2ed9e0db81908e4f60584d18ced16f0bde3450
skalluraya <skalluraya@gmail.com>
2025-03-25 21:09:35 -0700

    Create stock_with_max_profit.py

---
+++ stock_with_max_profit.py
@@ -0,0 +1,20 @@
+# best time to buy stock
+'''
+  You are given an array prices where prices[i] is the price of a given stock on the ith day. 
+  You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock. 
+  Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+'''
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        buy = prices[0]
+        profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] < buy:
+                buy = prices[i]
+            elif (prices[i] - buy) > profit:
+                profit = prices[i] - buy
+        return profit
