9e4362be88885b6a82698dd08760e5fd667754fc
skalluraya <skalluraya@gmail.com>
2025-03-31 20:50:10 -0700

    Create Longest_substring_3.py

---
+++ Longest_substring_3.py
@@ -0,0 +1,18 @@
+# Longest Substring Without Repeating Characters Leet code #3
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if len(s) <= 1:
+            return len(s)
+        char_index = {}  # Store last seen index of characters
+        left = 0  # Start of window
+        max_length = 0  # Result
+        for right in range(len(s)):
+            if s[right] in char_index and char_index[s[right]] >= left:
+                left = char_index[s[right]] + 1  # Move left past duplicate
+            char_index[s[right]] = right  # Update last seen index
+            max_length = max(max_length, right - left + 1)  # Update max_length    
+        return max_length
