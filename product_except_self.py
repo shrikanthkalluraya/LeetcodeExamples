ef5b564e3988a258aa41796e8511f0ade6770f84
skalluraya <skalluraya@gmail.com>
2025-04-07 07:05:48 -0700

    Create product_except_self.py

---
+++ product_except_self.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        # Product of Array Except Self
+        prefix = 1
+        output=[]
+        postfix = 1
+        output = [1] * len(nums)  # Initialize output array with 1s
+
+        # First pass: prefix products
+        for i in range(len(nums)):
+            output[i] = prefix
+            prefix = nums[i] * prefix
+
+        # Second pass: postfix products
+        for i in range(len(nums) - 1, -1, -1):
+            output[i] = output[i]*  postfix
+            postfix = nums[i] *   postfix
+        return output
+        
