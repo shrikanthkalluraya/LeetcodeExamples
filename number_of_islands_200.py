4841ae71aacc690c1477b76e76caaafda43cf430
skalluraya <skalluraya@gmail.com>
2025-04-29 06:04:45 -0700

    Create number_of_islands_200.py

---
+++ number_of_islands_200.py
@@ -0,0 +1,34 @@
+# Number of Islands LeetCode - 200
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if not grid:
+            return 0
+        
+        rows = len(grid)
+        cols = len(grid[0])
+        island_count = 0
+        
+        def dfs(r, c):
+            # Base case: if out of bounds or water, stop
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == '0':
+                return
+            
+            grid[r][c] = '0'  # Mark as visited (sink the island)
+
+            # Explore all 4 directions
+            dfs(r + 1, c)  # down
+            dfs(r - 1, c)  # up
+            dfs(r, c + 1)  # right
+            dfs(r, c - 1)  # left
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1':
+                    island_count += 1
+                    dfs(r, c)
+        
+        return island_count
