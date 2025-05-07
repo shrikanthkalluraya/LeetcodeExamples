0a4f1b866a632aaab00850a004907240d3038d0a
skalluraya <skalluraya@gmail.com>
2025-05-06 22:17:42 -0700

    Update and rename product_except_self.py to product_except_self_238.py

--- product_except_self_238.py
+++ product_except_self_238.py
@@ -1,3 +1,6 @@
+# 238. Product of Array Except Self
+# Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
 class Solution(object):
     def productExceptSelf(self, nums):
         """
