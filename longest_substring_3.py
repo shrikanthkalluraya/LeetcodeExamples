35fbcc368e6ae1b4da453d4c10cc691f8afc5d94
Shrikanth <skalluraya@gmail.com>
2025-08-03 21:38:18 -0700

    Update longest_substring_3.py

--- longest_substring_3.py
+++ longest_substring_3.py
@@ -18,3 +18,20 @@ class Solution(object):
             char_index[s[right]] = right  # Update last seen index
             max_length = max(max_length, right - left + 1)  # Update max_length    
         return max_length
+
+    def longest_substring_without_repeating_characters_using_number_of_occurence(self, s: str) -> int:
+        # WRITE YOUR BRILLIANT CODE HERE
+        left = 0
+        longest_sub_string = ""
+        substr_map = {}
+        max_len = 0
+        for right in range(0, len(s)):
+            substr_map[s[right]] = substr_map.get(s[right], 0) + 1 
+            while substr_map[s[right]] > 1:
+                substr_map[s[left]] -= 1
+                left += 1
+            curr_max_len = max_len
+            max_len = max(max_len, right-left+1)
+            if max_len!= curr_max_len:
+                longest_sub_string = s[left:right + 1]
+        return max_len
