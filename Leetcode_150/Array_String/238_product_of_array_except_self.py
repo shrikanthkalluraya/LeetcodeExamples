89fc94415c7a48d5e94ac6f12b71a20005eb2483
Shrikanth <skalluraya@gmail.com>
2025-11-22 19:15:37 -0700

    Add LeetCode 150: Array/String - Product of Array Except Self
    Implement product of array except self using prefix and postfix products.

---
+++ Leetcode_150/Array_String/238_product_of_array_except_self.py
@@ -0,0 +1,14 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prefix = 1
+        postfix = 1
+        result = [1] * len(nums)
+        for i in range(len(nums)):
+            result[i] = prefix
+            prefix *= nums[i]
+        
+        for i in range(len(nums) - 1, -1, -1):
+            result[i] *= postfix
+            postfix *= nums[i]
+        
+        return result
